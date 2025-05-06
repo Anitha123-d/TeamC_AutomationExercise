3f9eb2f31852c357f29485f61cebfa82bf3ee30c
Thummala aruna <Thummala aruna@LAPTOP-FNTVQIIL>
2025-05-06 16:24:57 +0530

    commit by Anitha

---
+++ AutomationExercise_Team_C_main/src/test/java/product_test/Testcase_28.java
@@ -0,0 +1,14 @@
+package product_test;
+
+import org.testng.annotations.Test;
+
+public class Testcase_28 {
+	public class Testcase_27 {
+		@Test
+		public void Anitha() {
+			System.out.println("learning github");
+		}
+		}
+
+
+}
